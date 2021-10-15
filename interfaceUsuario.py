6f5d273ca9710ebe3c98a6e9753590005a8aa2b8
ramirobarros <89500830+ramirobarros@users.noreply.github.com>
2021-10-15 03:09:30 -0300

    Update interfaceUsuario.py

--- interfaceUsuario.py
+++ interfaceUsuario.py
@@ -1,88 +1,27 @@
-from log import *
-import os
-
-class InterfaceUsuario:
+class Jogador:
     """
-        Super classe que inclui todos os métodos e atributos que são úteis para qualquer interação                          
-        que pode ser feita com o usuário.
+        Esta classe representa as informações do jogador.
     """
-    __atributos  = {}
-    __metodos    = {'__init__','__str__','pause','salva','novoJogo','limpaTela','getManual','getAtributos','getMetodos'}
+    __atributos  = {'nick', 'pontuacaoAtual','quantErros'}
+    __metodos    = {'__init__','__str__','getManual','getAtributos','getMetodos'}
 
-
-    def __init__(self):
-        """
-            Metodo construtor.
-        """
-        pass
-
+    
+    def __init__(self, nick = 'Jogador',pontuacaoAtual=0, quantErros=0):
+        self.nick = nick                        # Nome do jogador no jogo.
+        self.pontuacaoAtual = pontuacaoAtual    # Quantas posições o jogador já acertou até agora
+        self.quantErros = quantErros            # Quantas vezes o jogador errou
+        
     def __str__(self):
         """
             Este método, usado para representação em string, será retornada a String que contém as descrições de todos os métodos e 
             atributos das classes.
             
         """
-    
-    # Tabela com todos os caracteres reconhecidos pelo console do Windows.
-    tabelaCaracteres = {chr(x) : x for x in range(256)}
-
-    def menu(self):
-        """
-            Função que oferece opçoes para o usuário
-        """
-
-        print('Bem vindo ao Sudoku!')
-        print('Escolha uma opção:')
-        print('1) Novo Jogo')
-        print('2) Carregar Jogo ou Salvar jogo atual')
-        print('3) Estatísticas do jogo')
-        print('4) Sair')
-
-
-    def jogada(self):
-        """
-           Faz a jogada do 'jogador', ou seja, insere o valor escolhido na posição 'pos' 
-        """
-        while True:
-            try:
-                linha = int(input('Informe a posição da linha (1 a 9)'))
-                coluna = int(input('Informe a posição da coluna (1 a 9)'))
-                pos = [linha,coluna]
-            except ValueError:
-                print('Linha ou Coluna Inválida. Tente Novamente!')
-                Log().arquivoErros(self,'ValueError')
-            except CommandError:
-                print('Comando Inválido. Tente Novamente!')
-                Log().arquivoErros(self,'CommandError')
-
-
-
-    def pause():
-        """
-        Função que pausa o jogo
-        """
-    
-    def salvar():
-        """
-        Função que salva os dados do jogo até o momento
-        """
         pass
-
-    def novoJogo():
-        """
-        Função que cria um novo jogo
-        """
-        pass
-
-    def limpaTela():
-        """
-            Função que limpa toda a tela.
-        """
-        os.system('cls')
 
     def getAtributos():
         """
-            Esta função estática (chamada sempre através de InterfaceUsuario.getAtributos()) retorna um 
+            Esta função estática (chamada sempre através de Jogador.getAtributos()) retorna um 
             conjunto com os nomes dos atributos desta classe.
             
             (None) -> set
@@ -91,7 +30,7 @@ class InterfaceUsuario:
     
     def getMetodos():
         """
-            Esta função estática (chamada sempre através de InterfaceUsuario.getMetodos()) retorna um 
+            Esta função estática (chamada sempre através de Jogador.getMetodos()) retorna um 
             conjunto com os nomes dos métodos desta classe.
             
             (None) -> set
@@ -100,19 +39,16 @@ class InterfaceUsuario:
 
     def getManual():
         """
-            Esta função estática (chamada sempre através de InterfaceUsuario.getManual()) retorna um 
+            Esta função estática (chamada sempre através de Jogador.getManual()) retorna um 
             dicionário que mapeia os nomes dos atributos e métodos às suas descrições.
             
             (None) -> dict
         """
         manual = dict()
-        manual['__init__']              = InterfaceUsuario.__init__.__doc__
-        manual['__str__']               = InterfaceUsuario.__str__.__doc__
-        manual['pause']           = InterfaceUsuario.pause.__doc__
-        manual['salvar']     = InterfaceUsuario.salvar.__doc__
-        manual['novoJogo']             = InterfaceUsuario.novoJogo.__doc__
-        manual['limpaTela']            =InterfaceUsuario.limpaTela.__doc__
-        manual['getManual']          = InterfaceUsuario.getManual.__doc__
-        manual['getAtributos']         = InterfaceUsuario.getAtributos.__doc__
-        manual['getMetodos']            = InterfaceUsuario.getMetodos.__doc__
+        manual['__init__']              = Jogador.__init__.__doc__
+        manual['__str__']               = Jogador.__str__.__doc__
+        manual['getManual']          = Jogador.getManual.__doc__
+        manual['getAtributos']         = Jogador.getAtributos.__doc__
+        manual['getMetodos']            = Jogador.getMetodos.__doc__
         return manual
+
