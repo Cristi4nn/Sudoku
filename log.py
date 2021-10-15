5be0f1dde4271a78f83c5302d9d5937d6b13e375
ramirobarros <89500830+ramirobarros@users.noreply.github.com>
2021-10-15 03:10:20 -0300

    Update log.py

--- log.py
+++ log.py
@@ -1,9 +1,13 @@
+from jogador import *
+import matplotlib.pyplot as plt
+
+
 class Log:
     """
     Classe que conterá as estatística do jogo
     """
     __atributos  = {}
-    __metodos    = {'__init__','__str__','getManual','getAtributos','getMetodos'}
+    __metodos    = {'__init__','__str__','getManual','getAtributos','getMetodos','arquivo_jogadas','arquivo_erros','arquivo_save'}
     def __init__(self):
         """
             Metodo construtor
@@ -17,7 +21,6 @@ class Log:
             
         """
         pass
-    def 
         
 
     def getAtributos():
@@ -37,3 +40,53 @@ class Log:
             (None) -> set
         """
         return __metodos
+
+    def arquivoJogadas(self,jogada):
+        """
+            Esta função que registra as jogadas feitas pelo usuário do sudoku, salvando-as num arquivo'.txt'
+        """
+        arq= Jogador().nick + '_jogadas.txt'
+        
+        arquivo1=open(arq,'a')
+        arquivo1.write(jogada)
+        arquivo1.close()
+        
+    def arquivoErros(self,erros):
+        """
+            Esta função que registra as jogadas feitas pelo usuário do sudoku, salvando-as num arquivo'.txt'
+        """
+        arq= Jogador().nick + '_erros.txt'
+        
+        arquivo1=open(arq,'a')
+        arquivo1.write(erros)
+        arquivo1.close()
+
+    def arquivoSave(self,save):
+        """
+            Esta função que registra as jogadas feitas pelo usuário do sudoku, salvando-as num arquivo'.txt'
+        """
+        arq= Jogador().nick + '_save.txt'
+        
+        arquivo1=open(arq,'a')
+        arquivo1.write(save)
+        arquivo1.close()
+
+    def getManual():
+        """
+            Esta função estática (chamada sempre através de Log.getManual()) retorna um 
+            dicionário que mapeia os nomes dos atributos e métodos às suas descrições.
+            
+            (None) -> dict
+        """
+        manual = dict()
+        manual['__init__']              = Log.__init__.__doc__
+        manual['__str__']               = Log.__str__.__doc__
+        manual['arquivoJogadas']           = Log.arquivoJogadas.__doc__
+        manual['arquivoErros']     = Log.arquivoErros.__doc__
+        manual['arquivoSave']     = Log.arquivoSave.__doc__
+        manual['getManual']          = Log.getManual.__doc__
+        manual['getAtributos']         = Log.getAtributos.__doc__
+        manual['getMetodos']            = Log.getMetodos.__doc__
+        return manual
+
+        
